554da886c2cc7aa5eaf4664f49dcdf3deff19e1b
Barkasso <barkaso23@gmail.com>
2022-10-23 13:03:15 +0200

    Scanner learning in progress :)

---
+++ src/main/java/trzeci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class trzeci {
+   public static void main(String[] args){
+
+       Scanner scanner = new Scanner(System.in);
+
+       String name;
+       System.out.println("Podaj swoje immie: ");
+
+       name = scanner.next();
+       System.out.println("Hello " + name + " nice to meat you :D");
+
+   }
+}
